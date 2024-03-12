137ee7931a1855ca2723f53cf341bdb3a25a400e
ann-trann <122709489+ann-trann@users.noreply.github.com>
2024-03-12 17:36:23 +0700

    Generic

---
+++ java_66/TestBox.java
@@ -0,0 +1,15 @@
+package java_66;
+
+public class TestBox {
+
+	public static void main(String[] args) {
+		Box box = new Box(15);
+		System.out.println("Gia tri: " + box.getValue());
+		
+//		Box box = new Box(15.5); => Bao loi
+//		Box box = new Box("Muoi lam"); => Bao loi
+		
+		
+	}
+
+}
