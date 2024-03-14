d21030fb72187889ccf09e4c646f59e818c72a63
ann-trann <122709489+ann-trann@users.noreply.github.com>
2024-03-14 22:33:54 +0700

    save object to file

---
+++ java_73/src/main/ViDuGhiDoiTuong.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class ViDuGhiDoiTuong {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("D:\\java\\java_73\\file.data");
+			OutputStream os = new FileOutputStream(file);
+			ObjectOutputStream oos = new ObjectOutputStream(os);
+			
+			SinhVien st = new SinhVien("001", "Hello", 2000, 10);
+			oos.writeObject(st);
+			
+			oos.flush();
+			oos.close();
+		} catch (Exception e) {
+			
+		}
+
+	}
+
+}
